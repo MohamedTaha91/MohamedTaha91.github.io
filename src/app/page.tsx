38778b6f08705f96754080149b579b25f5379648
MohamedTaha91 <Taha_Acme@hotmial.com>
2024-05-08 16:49:54 +0200

    carousel logic implented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,79 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import webDev from "../../public/profilePictures/webDev.jpg";
 import webDchemicalEngineerev from "../../public/profilePictures/chemicalEngineer.jpg";
 import hobbies from "../../public/profilePictures/hobbies.jpg";
 import reactLogo from "../../public/Logos/reactLogo.png";
-import background from "./../../public/backgroundImage/background.png";
+
+const CONTENT = [
+  {
+    alt: "personal picture",
+    id: 0,
+    image: webDev,
+    heroImage: webDev,
+    title: "WebDev",
+    description:
+      "As a recent graduate from IronHack with a background inChemical Engineering, I have transitioned into the realm ofFull Stack Development. Passionate about crafting visuallycaptivating applications, I am committed to continuousimprovement in my skill set. Currently employed part-timeat Sloowy as an Associate Developer, I contribute to thedevelopment of an admin dashboard website using Next.JS.My collaborative spirit thrives in a team environment, whereI eagerly engage in solving intricate challenges. I take pridein assuming end-to-end responsibility for the features Idevelop, from initial design concepts to final delivery.",
+  },
+  {
+    alt: "picture of chemical plant",
+    id: 1,
+    image: webDchemicalEngineerev,
+    heroImage: webDchemicalEngineerev,
+    title: "Chemical Engineering",
+    description:
+      "Accumulated Problem Solving and solution implementation skills.\nImplemented Process Design and simulation creation using software for petroleum plants.\n Created Process Schemes , Layouts and Documentation Creation,\n Successfully contributed to closing Purchases with Suppliers and Clients by assigning budgets.",
+  },
+  {
+    alt: "picture of me singing",
+    id: 2,
+    image: hobbies,
+    heroImage: hobbies,
+    title: "hobbies",
+    description: "Sports , Gaming, Music, Traveling , Voice Acting",
+  },
+];
 
 export default function Home() {
+  const [title, setTitle] = useState(CONTENT[0].title);
+  const [heroImage, setheroImage] = useState(CONTENT[0].heroImage);
+  const [description, setDescription] = useState(CONTENT[0].description);
+
+  const handleOnClick = (id: number) => {
+    const selectedElement = CONTENT[id];
+    setTitle(selectedElement.title);
+    setheroImage(selectedElement.heroImage);
+    setDescription(selectedElement.description);
+  };
+
   return (
     <div className="details">
       <div className="carousel">
         <Image
           className="heroImage"
-          src={reactLogo}
+          src={heroImage}
           alt="hero image"
           width={230}
           height={200}
         ></Image>
         <div className="smallCarousel">
-          <Image
-            className="webDev"
-            src={webDev}
-            alt="webDev profile picture"
-            height={60}
-            width={60}
-          />
-          <Image
-            className="chemicalEngineering"
-            src={webDchemicalEngineerev}
-            alt="webDev profile picture"
-            height={60}
-            width={60}
-          />
-          <Image
-            className="hobbies"
-            src={hobbies}
-            alt="webDev profile picture"
-            height={60}
-            width={60}
-          />
+          {CONTENT.map((category) => {
+            return (
+              <Image
+                onClick={() => handleOnClick(category.id)}
+                className="webDev"
+                src={category.image}
+                alt={category.alt}
+                height={85}
+                width={85}
+              />
+            );
+          })}
         </div>
       </div>
-      <div className="description-Title">WebDev</div>
-      <div className="description-Text">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-        dolor, beatae illum atque error similique nostrum ut maxime deleniti
-        iure sapiente, itaque quidem incidunt assumenda ea, a recusandae minima?
-        Facere!rem ipsum
-      </div>
+      <div className="description-Title">{title}</div>
+      <div className="description-Text">{description}</div>
     </div>
   );
 }
